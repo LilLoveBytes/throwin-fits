57b6b76c8d6488984817e17d35966981a9b74bb9
La Tasha Pollard <latashajpollard@gmail.com>
2024-02-01 13:43:32 -0600

    Initial commit - database, user model, and sign up functional

---
+++ BackEnd/app/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager, create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+
+    jwt = JWTManager(app)
+
+    from app.models.user import User
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from .routes.userroutes import user_bp
+    app.register_blueprint(user_bp)
+
+    CORS(app)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True, host='0.0.0.0')
---
+++ BackEnd/app/models/user.py
@@ -0,0 +1,14 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
+  password = db.Column(db.String, nullable=False)
+
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+  
+  def check_password(self, password):
+    self.password = check_password_hash(self.password, password)
